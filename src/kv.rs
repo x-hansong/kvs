bb9467ccf7fb62e107d6a18073b0e6206abf044c
hansong.xhs <hansong.xhs@gmail.com>
2021-02-12 10:16:57 +0800

    temp save

--- src/kv.rs
+++ src/kv.rs
@@ -1,18 +1,9 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
+use crate::{Result, KvsError};
+use serde::{Serialize, Deserialize};
 
 /// The `KvStore` stores string key/value pairs.
-///
-/// Key/value pairs are stored in a `HashMap` in memory and not persisted to disk.
-///
-/// Example:
-///
-/// ```rust
-/// # use kvs::KvStore;
-/// let mut store = KvStore::new();
-/// store.set("key".to_owned(), "value".to_owned());
-/// let val = store.get("key".to_owned());
-/// assert_eq!(val, Some("value".to_owned()));
-/// ```
 #[derive(Default)]
 pub struct KvStore {
     map: HashMap<String, String>
@@ -20,29 +11,37 @@ pub struct KvStore {
 
 impl KvStore {
 
-    /// create a `KvStore`
-    pub fn new() -> KvStore {
-        KvStore {
-            map: HashMap::new()
-        }
+    /// Open a `KvStore` with the given path.
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        Ok(KvStore::new())
     }
 
     /// Sets the value of a string key to a string.
     ///
     /// If the key already exists, the previous value will be overwritten.
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.map.insert(key, value);
+        Ok(())
     }
 
     /// Gets the string value of a given string key.
     ///
     /// Returns `None` if the given key does not exist.
-    pub fn get(&self, key: String) -> Option<String> {
-        self.map.get(key.as_str()).cloned()
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        Ok(self.map.get(key.as_str()).cloned())
     }
 
     /// Remove a given key.
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.map.remove(key.as_str());
+        Ok(())
     }
+
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Command {
+    Set {key: String, value: String},
+    Remove {key: String}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@
 //! A simple key/value store.
 
 pub use kv::KvStore;
+pub use error::{KvsError, Result};
 
 mod kv;
+mod error;
